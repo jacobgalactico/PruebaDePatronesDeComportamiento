84305c112477bb63f1d3e86eec7d4adfa9a9954d
jacobgalactico <jaltevil@myuax.com>
2024-04-29 15:35:36 +0200

    Clase CountUp. Cuenta del 1 al infinito.

---
+++ src/CountUp.java
@@ -0,0 +1,12 @@
+public class CountUp {
+
+    public static void main(String[] args) {
+        int num = 1; // Este es el punto de partida del conteo ascendente.
+
+        while (true) { // Un bucle infinito, nunca se detiene.
+            System.out.println(num);
+            num++; // Incrementamos el número en 1.
+        }
+
+    }
+}
