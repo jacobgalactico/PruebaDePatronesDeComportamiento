7cf284853328ceaad18fabd0f8ef4851d155cbe9
jacobgalactico <jaltevil@myuax.com>
2024-05-02 22:55:11 +0200

    Creación de los test.

---
+++ test/HaltCheckerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HaltCheckerTest {
+
+    @Test
+    void willHalt() {
+    }
+
+    @Test
+    void main() {
+    }
+}
