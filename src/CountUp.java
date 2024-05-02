b33ad87f270efdf0d50e1a47808bfae523cd8ea8
jacobgalactico <jaltevil@myuax.com>
2024-05-02 22:54:37 +0200

    .

--- src/CountUp.java
+++ src/CountUp.java
@@ -1,11 +1,11 @@
 public class CountUp {
 
     public static void main(String[] args) {
-        int num = 1; // Este es el punto de partida del conteo ascendente.
+        int num = 1;
 
         while (true) { // Un bucle infinito, nunca se detiene.
             System.out.println(num);
-            num++; // Incrementamos el número en 1.
+            num++;
         }
 
     }
