242c7d937e715574466118cce274f979c8bb672d
jacobgalactico <jaltevil@myuax.com>
2024-05-04 16:04:13 +0200

    Test

---
+++ test/swing/MenuInteractivoGUITest.java
@@ -0,0 +1,12 @@
+package swing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuInteractivoGUITest {
+
+    @Test
+    void main() {
+    }
+}
