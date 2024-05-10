9fb12487b71cef90254c1fe76aaac55f4f670b27
jacobgalactico <jaltevil@myuax.com>
2024-05-10 21:11:50 +0200

    JAVADOC

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"clases","l":"CountDown"},{"p":"clases","l":"CountUp"},{"p":"clases","l":"HaltChecker"},{"p":"clases.strategy","l":"HaltStrategy"},{"p":"<Unnamed>","l":"MenuInteractivoGUI"},{"p":"clases","l":"Reverser"},{"p":"clases.strategy","l":"TimeHaltStrategy"}];updateSearchResults();
